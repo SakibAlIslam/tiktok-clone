e295f6314362c34ca43433ca471082a7f9dfcaf8
DripDevelopment <106390882+DripDevelopment@users.noreply.github.com>
2022-11-10 04:00:01 +0600

    navbar initialization and logo added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -11,6 +13,18 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   if (isSSR) return null;
 
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+          <Sidebar />
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[82vh] videos flex-1'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
 export default App;
